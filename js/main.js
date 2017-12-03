9362f7945f90bfbbd704f482162e21dacb4b9f2a
Dawit <xp0sr@users.noreply.github.com>
2017-12-02 22:48:50 -0600

    Add image to array version

--- js/main.js
+++ js/main.js
@@ -3,15 +3,14 @@ $(document).ready(function() {
   var $authorPhoto = $("#author-photo");
   var $authorName = $("#author-name");
   var quoteArr = [
-    ['Success is the sum of small efforts, repeated day in and day out.','Robert J. Collier'],
-    ['Just as man cannot live without dreams, he cannot live without hope. If dreams reflect the past, hope summons the future.', 'Elie Wiesel'],
-    ['Being Yourself Always Plays Out.', 'Garyvee'],
-    ['If you want a thing done well, do it yourself.', 'Napoleon Bonaparte'],
-    ['Art is making something out of nothing and selling it.', 'Frank Zappa'],
-    ['If the path be beautiful, let us not ask where it leads.', 'Anatole France'],
-    ['Life in abundance comes only through great love.', 'Elbert Hubbard'],
-    ['Excuses are the currency that allow you to not act.', 'Garyvee']
-    ['Imagination is more important than knowledge.', 'Albert Einstein']
+    ['collier.jpg','Success is the sum of small efforts, repeated day in and day out.','Robert J. Collier'],
+    ['wiesel.jpg','Just as man cannot live without dreams, he cannot live without hope. If dreams reflect the past, hope summons the future.', 'Elie Wiesel'],
+    ['garyvee.jpeg','Being Yourself Always Plays Out.', 'Garyvee'],
+    ['zappa.jpg','Art is making something out of nothing and selling it.', 'Frank Zappa'],
+    ['france.jpg','If the path be beautiful, let us not ask where it leads.', 'Anatole France'],
+    ['hubbard.jpg','Life in abundance comes only through great love.', 'Elbert Hubbard'],
+    ['garyvee.jpeg','Excuses are the currency that allow you to not act.', 'Garyvee'],
+    ['einstein.jpg','Imagination is more important than knowledge.', 'Albert Einstein']
   ];
 
   $.ajax({
@@ -45,18 +44,20 @@ $(document).ready(function() {
     error(jqXHR,status,errorThrown) {
 
       var $quoteBtn = $('#generate-btn');
-      $authorPhoto.css('background-image','url(data/brain.jpg)');
 
       $quoteBtn.on('click',function() {
-        var rndm = Math.floor(Math.random()*quoteArr.length);
-        $authorQuote.html(quoteArr[rndm][0]);
-        $authorName.html(quoteArr[rndm][1]);
-        quoteArr.splice(rndm,1);
+        var random = Math.floor(Math.random()*quoteArr.length);
+        var photo = quoteArr[random][0];
+        $authorPhoto.css('background-image','url(data/' + photo + ')');
+        $authorQuote.html(quoteArr[random][1]);
+        $authorName.html(quoteArr[random][2]);
+        quoteArr.splice(random,1);
 
         if (quoteArr.length < 1) {
 
           $authorQuote.html('Sorry, No More Quotes.<br>');
           $authorName.html('Please Reload to View Again.');
+          $authorPhoto.css('background-image','url(data/brain.jpg)');
 
         }
       });
