c836be837dfddfed802c880c7e5f4c01766d53dc
Dawit <xp0sr@users.noreply.github.com>
2018-01-16 16:47:39 -0600

    API integration and Tweet button added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   var $authorQuote = $("#quote");
-  var $authorPhoto = $("#author-photo");
   var $authorName = $("#author-name");
+  var $authorName = $("#author-photo");
+  const tweet = document.querySelector('#tweet');
   var quoteArr = [
     ['collier.jpg','Success is the sum of small efforts, repeated day in and day out.','Robert J. Collier'],
     ['wiesel.jpg','Just as man cannot live without dreams, he cannot live without hope. If dreams reflect the past, hope summons the future.', 'Elie Wiesel'],
@@ -13,59 +14,35 @@ $(document).ready(function() {
     ['einstein.jpg','Imagination is more important than knowledge.', 'Albert Einstein']
   ];
 
-  $.ajax({
-    url: 'data/data.json',
-    type: 'GET',
-    dataType: 'json',
-    success(response) {
+  $('#generate-btn').click(function() {
 
-      $('#generate-btn').click(function() {
-        var name, photo;
-        var eachData = [];
-        if ($.isEmptyObject(response)) {
-          $authorQuote.html('Sorry, No More Quotes.<br>');
-          $authorName.html('Please Reload to View Again.');
-          $authorPhoto.css('background-image','url(data/brain.jpg)');
+    $.ajax({
+      url: 'https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?',
+      type: 'GET',
+      dataType: 'jsonp',
+      success(response) {
+        $('#quote').html(response.quoteText);
+        $('#author-name').html(response.quoteAuthor);
+        if (response.quoteAuthor) {
+          tweet.href = `https://twitter.com/home/?status=${response.quoteText} -${response.quoteAuthor}`;
+        } else {
+          tweet.href = `https://twitter.com/home/?status=${response.quoteText}`;
         }
-
-        for (name in response) {
-          eachData = response[name];
-          photo = eachData[0];
-          quote = eachData[1];
-          $authorQuote.html(quote);
-          $authorName.html(name);
-          $authorPhoto.css('background-image','url(data/' + photo + ')');
-          delete response[name];
-          break;
-        }
-      });
-
-    },
-    error(jqXHR,status,errorThrown) {
-
-      var $quoteBtn = $('#generate-btn');
-
-      $quoteBtn.on('click',function() {
+      },
+      error(jqXHR,status,errorThrown) {
         var random = Math.floor(Math.random()*quoteArr.length);
         var photo = quoteArr[random][0];
         $authorPhoto.css('background-image','url(data/' + photo + ')');
         $authorQuote.html(quoteArr[random][1]);
         $authorName.html(quoteArr[random][2]);
+        tweet.href = `https://twitter.com/home/?status=${quoteArr[random][0]} -${quoteArr[random][1]}`;
         quoteArr.splice(random,1);
-
         if (quoteArr.length < 1) {
-
           $authorQuote.html('Sorry, No More Quotes.<br>');
           $authorName.html('Please Reload to View Again.');
           $authorPhoto.css('background-image','url(data/brain.jpg)');
-
         }
-      });
-    }
+      }
+    });
   });
-
-
-
-
-
 });
